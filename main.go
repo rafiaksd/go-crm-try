24e23ce9e76103a081d0c77cb9cd9dc014262ba2
An0nym0usShad0w <76418474+rafiaksd@users.noreply.github.com>
2025-12-07 14:47:57 +0600

    new structs

--- main.go
+++ main.go
@@ -2,7 +2,26 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+type Customer struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	CreatedAt time.Time
+}
+
+type CRM struct {
+	customers map[int]Customer
+	NextID    int
+}
+
+func NewCRM() *CRM {
+	return &CRM{customers: make(map[int]Customer), NextID: 1}
+}
 
 func main() {
 	fmt.Println("First push")
